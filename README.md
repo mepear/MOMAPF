5b141e888686c1920cc467253142a6d3f6de1947
mepear <1064932425@qq.com>
2022-04-04 20:15:53 -0700

    simple_replicate

---
+++ README.md
@@ -0,0 +1,20 @@
+# public_pymomapf
+This repo contains the Python implementation of algorithms (NAMOA*, MOM*, MO-CBS) related to a multi-objective multi-agent path finding (MOMAPF) problem.
+The main purpose of the code is to illustrate and verify the algorithm, and is not optimized for performance.
+Script run_example.py can serve as an entry point to the planners.
+Feel free to use the code for research purposes.
+This repo is still under construction and more contents will be added.
+
+Papers and websites related to this repo:
+
+* [1] [Homepage](https://wonderren.github.io/)
+
+* [2] [Multi-objective Conflict-based Search for Multi-agent Path Finding](https://arxiv.org/pdf/2101.03805.pdf).\
+	**Zhongqiang Ren**, Sivakumar Rathinam, and Howie Choset.\
+	<i>IEEE International Conference on Robotics and Automation (ICRA)</i>, 2021.\
+	[[bibtex](https://wonderren.github.io/files/bibtex_ren21mocbs.txt)] 
+
+* [3] [Subdimensional Expansion for Multi-objective Multi-agent Path Finding](https://arxiv.org/pdf/2102.01353.pdf).\
+	**Zhongqiang Ren**, Sivakumar Rathinam, and Howie Choset.\
+	<i>IEEE Robotics and Automation Letters (RA-L)</i>, 2021.\
+	[[bibtex](https://wonderren.github.io/files/bibtex_ren21momstar.txt)] 
---
+++ run_example.py
@@ -0,0 +1,71 @@
+"""
+Author: Zhongqiang (Richard) Ren
+Version@2021
+Remark: Entry point to planners.
+"""
+import context
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import common 
+import mocbs
+import moastar # NAMOA*
+import momstar
+
+
+def RunToyExample():
+  """
+  """
+
+  # a tiny world with very simple cost vectors.
+
+  grids = np.zeros((3,3))
+  grids[0,:]=1
+  grids[1,0]=1
+  grids[1,2]=1
+
+  sy = np.array([2,2]) # start y = rows in grid image, the kth component corresponds to the kth robot.
+  sx = np.array([0,2]) # start x = column in grid image
+  gy = np.array([2,2]) # goal y
+  gx = np.array([2,0]) # goal x
+
+  cvecs = [np.array([2,2]), np.array([0,5])] # the kth component corresponds to the kth robot.
+  cgrids = [np.ones((3,3)), np.ones((3,3))] # the mth component corresponds to the mth objective.
+  # cost for agent-i to go through an edge c[m] = cvecs[i][m] * cgrids[m][vy,vx], where vx,vy are the target node of the edge.
+
+  cdim = len(cvecs[0])
+
+  ##################################################################
+  #### choose one of the planner to run by uncommenting the code ###
+  ##################################################################
+
+  #### Invoke MO-CBS planner ###
+  res = mocbs.RunMocbsMAPF(grids, sx, sy, gx, gy, cvecs, cgrids, cdim, 1.0, 0.0, np.inf, 10, 2)
+
+  #### Invoke NAMOA* planner ###
+  # res = moastar.RunMoAstarMAPF(grids, sx, sy, gx, gy, cvecs, cgrids, cdim, 1.0, 0.0, np.inf, 10)
+
+  #### Invoke MOM* planner ###
+  # res = momstar.RunMoMstarMAPF(grids, sx, sy, gx, gy, cvecs, cgrids, cdim, 1.0, 0.0, np.inf, 10)
+
+  print(res)
+  
+  return
+
+
+def main():
+  """
+  """
+  RunToyExample()
+  return
+
+
+if __name__ == '__main__':
+  print("begin of main")
+  main()
+  print("end of main")
+
+
+  
